39398fd45bd957326a6661432956958966d52148
itonylaw <itonylaw@gmail.com>
2020-06-15 18:16:39 +0800

    example 01 String

---
+++ ex01-string.py
@@ -0,0 +1,21 @@
+# This is the comment
+
+# 字符串(String)
+
+#1
+print('"I don\'t know" they said.')
+
+#2
+s='First line. \nSecond line.'
+print(s)
+
+#3
+print('C:\some\name') # \n means newline
+print(r'C:\some\name')
+
+#4
+print("""\
+This is First line
+        and Second line
+""")
+
