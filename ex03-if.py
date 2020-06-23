554a5b860052c6de937be2bd83e089be4fd95418
itonylaw <itonylaw@gmail.com>
2020-06-23 12:33:34 +0800

    example 03

---
+++ ex03-if.py
@@ -0,0 +1,22 @@
+"""
+File: ex03-if.py
+Author: TonyDeep
+Date: 2020-06-23
+"""
+
+import random
+
+x = random.randint(1, 100)
+y = int(input('Please enter an integer(1-100):'))
+
+if y < 0 or y > 100:
+    print('out of range')
+elif x < y:
+    print(f'{x}<{y}')
+    print('You Win')
+elif x > y:
+    print(f'{x}>{y}')
+    print('You Loss')
+else:
+    print(f'{x}={y}')
+    print('Super')
