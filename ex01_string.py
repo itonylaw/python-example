eaaaf212f3d054c0db1898df1ca0e504daa2b139
itonylaw <itonylaw@gmail.com>
2020-07-15 11:10:39 +0800

    change file name

---
+++ ex01_string.py
@@ -0,0 +1,31 @@
+"""
+File: ex01_string.py
+Author: TonyDeep
+Date: 2020-06-20
+"""
+
+# This is the comment
+
+print('#0 字符串(String)\n')
+
+print('#1')
+print('"I don\'t know" they said.' )
+print('\n')
+
+print('#2')
+s='First line. \nSecond line.'
+print(s)
+print('\n')
+
+print('#3')
+print('C:\some\name') # \n means newline
+print(r'C:\some\name') #直出
+print('\n')
+
+print('#4')
+print("""\
+This is First line
+        and Second line
+""")
+
+
