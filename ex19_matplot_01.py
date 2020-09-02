9fe99507d582d20eb583628d01dc88bc92f955cf
itonylaw <itonylaw@gmail.com>
2020-09-02 16:09:27 +0800

    Matplotlib

---
+++ ex19_matplot_01.py
@@ -0,0 +1,15 @@
+"""
+File: ex17_matplot_01.py
+Author: TonyDeep
+Date: 2020-09-02
+"""
+
+import matplotlib.pyplot as plt
+# plt.plot([1,2,3,4])
+# plt.show()
+
+plt.axis([0, 5, 0, 20])
+plt.title('TonyDeep.com')
+plt.plot([1,2,3,4],[1,4,9,16],'ro')
+plt.show()
+
