e8b1b5c2e0ed938ec86b3f1665cce1c67f5eff7f
itonylaw <itonylaw@gmail.com>
2020-09-02 16:39:21 +0800

    matplot

--- ex19_matplot_01.py
+++ ex19_matplot_01.py
@@ -9,7 +9,15 @@ import matplotlib.pyplot as plt
 # plt.show()
 
 plt.axis([0, 5, 0, 20])
-plt.title('TonyDeep.com')
+plt.title('TonyDeep.com', fontsize=20, fontname='Times New Roman')
+plt.xlabel('Counting', color = 'gray')
+plt.ylabel('Square values', color = 'gray')
+plt.text(1, 1.5, 'First')
+plt.text(2, 4.5, 'second')
+plt.text(3, 9.5, 'Third')
+plt.text(4, 16.5, 'Fourth')
+plt.text(1.1, 12, r'$y=x^2', fontsize=20)
+plt.grid(True, linestyle='--', alpha=0.5)
 plt.plot([1,2,3,4],[1,4,9,16],'ro')
 plt.show()
 
